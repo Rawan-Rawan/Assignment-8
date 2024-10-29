33d3d373ec5472dab435c6582b3ec8fb123f83ee
Rawan <rawansalahat1@gmail.com>
2024-10-29 20:51:37 +0200

    done

--- Assets/Script/Script 2.cs
+++ Assets/Script/Script 2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class script3 : MonoBehaviour
+public class script2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
