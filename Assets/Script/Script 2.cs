e1657ad732f0bba64409c5e8d6a01583eb30981d
Rawan <rawansalahat1@gmail.com>
2024-10-29 20:51:02 +0200

    final

--- Assets/Script/Script 2.cs
+++ Assets/Script/Script 2.cs
@@ -11,7 +11,7 @@ public class script3 : MonoBehaviour
      int funnyArrayIndex =Random.Range(0,9);
      string space=" ";
      string funnySentence;
-     Debug.Log(funnySentence=funnyArray[0]+space+funnyArray[1]+space+funnyArray[2]+space+funnyArray[3]+space+funnyArray[4]+space+funnyArray[5]+space+funnyArray[6]);
+     Debug.Log(funnySentence=funnyArray[funnyArrayIndex]+space+funnyArray[funnyArrayIndex]+space+funnyArray[funnyArrayIndex]+space+funnyArray[funnyArrayIndex]+space+funnyArray[funnyArrayIndex]+space+funnyArray[funnyArrayIndex]+space+funnyArray[funnyArrayIndex]);
     }
 
     // Update is called once per frame
