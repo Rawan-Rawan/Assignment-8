1c1d1503896def1080268f3e689d74835fb42ea6
Rawan <rawansalahat1@gmail.com>
2024-10-29 20:55:39 +0200

    Done

--- Assets/Script/Script 2.cs
+++ Assets/Script/Script 2.cs
@@ -8,10 +8,17 @@ public class script2 : MonoBehaviour
     void Start()
     {
      string [] funnyArray ={"Cat", "Dog", "Car", "Pizza", "Hat", "Fish", "Tree", "Monkey", "Ball", "Bird"};
-     int funnyArrayIndex =Random.Range(0,9);
+     int funnyArrayIndex1 =Random.Range(0,9);
+     int funnyArrayIndex2 =Random.Range(0,9);
+     int funnyArrayIndex3 =Random.Range(0,9);
+     int funnyArrayIndex4 =Random.Range(0,9);
+     int funnyArrayIndex5 =Random.Range(0,9);
+     int funnyArrayIndex6 =Random.Range(0,9);
+     int funnyArrayIndex7 =Random.Range(0,9);
+     
      string space=" ";
      string funnySentence;
-     Debug.Log(funnySentence=funnyArray[funnyArrayIndex]+space+funnyArray[funnyArrayIndex]+space+funnyArray[funnyArrayIndex]+space+funnyArray[funnyArrayIndex]+space+funnyArray[funnyArrayIndex]+space+funnyArray[funnyArrayIndex]+space+funnyArray[funnyArrayIndex]);
+     Debug.Log(funnySentence=funnyArray[funnyArrayIndex1]+space+funnyArray[funnyArrayIndex2]+space+funnyArray[funnyArrayIndex3]+space+funnyArray[funnyArrayIndex4]+space+funnyArray[funnyArrayIndex5]+space+funnyArray[funnyArrayIndex6]+space+funnyArray[funnyArrayIndex7]);
     }
 
     // Update is called once per frame
