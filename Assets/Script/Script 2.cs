62a05cd1fa195422d7bc9ba2362192d32738ab30
Rawan <rawansalahat1@gmail.com>
2024-10-29 10:02:43 +0200

    New script

---
+++ Assets/Script/Script 2.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Script2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    string[] funnyArray = {"Cat", "Dog", "Car", "Pizza", "Hat", "Fish", "Tree", "Monkey", "Ball", "Bird"};
+    System.Random randomArray = new System.Random();
+    string funnySentence ="";
+    int wordCounter =0;
+    while (wordCounter<7)
+    { 
+    int randomArrayIndex = randomArray.Next (funnyArray.Length);
+    funnySentence += funnyArray[randomArrayIndex] + " ";
+    ++wordCounter;    
+    }
+    funnySentence = funnySentence.TrimEnd() + ".";
+    Debug.Log (funnySentence);
+    }
+}  
